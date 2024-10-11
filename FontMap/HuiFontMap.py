0ab6e032c87473f4fe7202428c1bced8e05f21fb
HuiHui-lin <2176189493@qq.com>
2024-10-11 21:02:23 +0800

    1

--- FontMap/HuiFontMap.py
+++ FontMap/HuiFontMap.py
@@ -11,7 +11,7 @@ class HuiFontMap:
     def __init__(self,fontpath,savepath,issaveimg=True):
        """
        初始化读取字体和获取字体文件映射
-       :param font: 字体文件路径
+       :param fontpath: 字体文件路径
        :param savepath:字形图片保存路径
        :param issaveimg:是否开启保存图片
        """
@@ -19,8 +19,8 @@ class HuiFontMap:
        self.savepath = savepath
        self.cmap = self.font.getBestCmap()
        if issaveimg:
-        self.saveFontImg(cmap=self.cmap,savepath=savepath)
-    def saveFontImg(self,cmap,savepath):
+        self.__saveFontImg(cmap=self.cmap,savepath=savepath)
+    def __saveFontImg(self,cmap,savepath):
         """
         根据字体文件生成对应字形图像并保存
         :param cmap:字符映射字典
@@ -39,44 +39,46 @@ class HuiFontMap:
             proportional_size= self.__get_proportional_size(original_width,original_height)
             resizeimg = fontimg.resize(proportional_size, Image.Resampling.LANCZOS)
             old_size = resizeimg.size
-            new_size = (400,400)
-            red_background = Image.new("RGBA", (400,400), 'red') #红色背景
+            new_size = (200,200)
+            red_background = Image.new("RGBA", (200,200), 'red') #红色背景
             red_background.paste(resizeimg, (int((new_size[0] - old_size[0]) / 2),
                                   int((new_size[1] - old_size[1]) / 2)),resizeimg.split()[1])
             red_background.save(os.path.join(savepath ,cmap[i] + '.png'))
 
-        print('字形图片保存完毕')
+        # print('字形图片保存完毕')
     def ocrFontImg(self,jsonpath = None):
         """
         :param jsonpath: ocr字典结果的保存路径，默认不保存
         :return: 返回一个ocr识别字典结果
         """
         import ddddocr
-        import unicodedata
         ocr = ddddocr.DdddOcr()
         file_list = os.listdir(self.savepath)
         dis  = {}
         reversed_dict = {value: key for key, value in self.cmap.items()}
+        ocrfail = {}
         for i in file_list:
             imgname = i.split('.')[0]
             if imgname in reversed_dict:
-                try:
-                    unicodedata.name(chr(reversed_dict[imgname]))
-                    dis[imgname] = chr(reversed_dict[imgname])
-                    continue
-                except ValueError:
                     path = os.path.join(self.savepath,i)
                     with open(path, 'rb') as f:
                         img_bytes = f.read()
                         res = ocr.classification(img_bytes)
+                    if res == '':
+                        dis[imgname] = 'Null'
+                        ocrfail[imgname] = 'Null'
+                    else:
                         dis[imgname]=res
             else:
                 continue
-        print('识别完毕')
+        # print('识别完毕')
+        if len(ocrfail)!=0:
+            print(f'由于ddddocr识别没有识别到,已为您打印失败结果：{ocrfail}')
+            print('如果您有好的解决办法,请联系我QQ:2176189493')
         if jsonpath!= None:
             with open(jsonpath, 'w', encoding='utf-8') as f:
                 json.dump(dis,fp=f,ensure_ascii=False)
-            print('文件已保存')
+            # print('文件已保存')
         return dis
     def __get_proportional_size(self,original_width, original_height):
         """
@@ -99,6 +101,4 @@ class HuiFontMap:
 if __name__ == '__main__':
     hui = HuiFontMap(fontpath=r"C:\Users\21761\Desktop\file.woff",savepath=r"C:\Users\21761\Desktop\新建文件夹",issaveimg=False)
     diststr = hui.ocrFontImg()
-
-
 
